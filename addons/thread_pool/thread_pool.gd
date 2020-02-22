8b5ac377da1f96a539644af73095d301145c4b2e
Marcos Zolnowski <marcos.zolnowski@gmail.com>
2020-02-22 01:39:58 -0300

    Adds mechanisms for fetching completed tasks

--- addons/thread_pool/thread_pool.gd
+++ addons/thread_pool/thread_pool.gd
@@ -7,27 +7,56 @@ var started = false
 var finished = false
 var tasks_lock = Mutex.new()
 var tasks_wait = Semaphore.new()
+var __finished_tasks = []
+var __finished_tasks_lock = Mutex.new()
+export var discard_finished_tasks: bool = false
+signal task_finished(task_tag)
+signal task_discarded(task)
 
 class Task:
-	var object_this: Object
-	var method_of_this: String
-	var userdata_argument
+	var target_instance: Object
+	var target_method: String
+	var target_argument
+	var result
+	var tag
+	var __no_param: bool
+	var __array_param: bool
 
-	func _init(instance: Object, method: String, userdata = null):
-		object_this = instance
-		method_of_this = method
-		userdata_argument = userdata
+	func _init(instance: Object, method: String, parameter, task_tag, no_param, array_param):
+		target_instance = instance
+		target_method = method
+		target_argument = parameter
+		result = null
+		tag = task_tag
+		__no_param = no_param
+		__array_param = array_param
 
-	func execute_task():
-		object_this.call(method_of_this, userdata_argument)
+	func __execute_task():
+		if __no_param:
+			result = target_instance.call(target_method)
+		elif __array_param:
+			result = target_instance.callv(target_method, target_argument)
+		else:
+			result = target_instance.call(target_method, target_argument)
 
-
-func submit_task(instance: Object, method: String, userdata = null) -> void:
+func __enqueue_task(instance: Object, method: String, parameter = null, task_tag = null, no_param = false, array_param = false) -> void:
 	tasks_lock.lock()
-	tasks.push_front(Task.new(instance, method, userdata))
+	tasks.push_front(Task.new(instance, method, parameter, task_tag, no_param, array_param))
 	tasks_wait.post()
 	__start()
 	tasks_lock.unlock()
+
+
+func submit_task(instance: Object, method: String, parameter, task_tag = null) -> void:
+	__enqueue_task(instance, method, parameter, task_tag, false, false)
+
+
+func submit_task_unparameterized(instance: Object, method: String, task_tag = null) -> void:
+	__enqueue_task(instance, method, null, task_tag, true, false)
+
+
+func submit_task_array_parameterized(instance: Object, method: String, parameter: Array, task_tag = null) -> void:
+	__enqueue_task(instance, method, parameter, task_tag, false, true)
 
 
 func shutdown():
@@ -63,8 +92,7 @@ func __create_pool():
 func __start() -> void:
 	if not started:
 		for t in pool:
-			#(t as Thread).start(self, "__execute_tasks")
-			(t as Thread).start(self, "__execute_tasks", t)# the thread as argument
+			(t as Thread).start(self, "__execute_tasks", t)
 		started = true
 
 
@@ -72,7 +100,7 @@ func __drain_task() -> Task:
 	tasks_lock.lock()
 	var result
 	if tasks.empty():
-		result = Task.new(self, "do_nothing")# normally, this is not expected, but better safe than sorry
+		result = Task.new(self, "do_nothing", null, null, true, false)# normally, this is not expected, but better safe than sorry
 	else:
 		result = tasks.pop_back()
 	tasks_lock.unlock()
@@ -80,16 +108,47 @@ func __drain_task() -> Task:
 
 
 func __execute_tasks(arg_thread) -> void:
-	#print(arg_thread)
+	#print_debug(arg_thread)
 	while not finished:
 		tasks_wait.wait()
 		var task = __drain_task() as Task
-		task.execute_task()
+		task.__execute_task()
+		if discard_finished_tasks:
+			call_deferred("emit_signal", "task_discarded", task)
+		else:
+			__finished_tasks_lock.lock()
+			__finished_tasks.append(task)
+			__finished_tasks_lock.unlock()
+			call_deferred("emit_signal", "task_finished", task.tag)
 
 
 func do_nothing(arg) -> void:
 	#print("doing nothing")
-	OS.delay_msec(1) # if there is nothing to do, go to sleep
+	OS.delay_msec(1) # if there is nothing to do, go sleep
+
+
+func fetch_finished_tasks() -> Array:
+	__finished_tasks_lock.lock()
+	var result = __finished_tasks
+	__finished_tasks = []
+	__finished_tasks_lock.unlock()
+	return result
+
+
+func fetch_finished_tasks_by_tag(tag) -> Array:
+	__finished_tasks_lock.lock()
+	var result = []
+	var new_finished_tasks = []
+	for t in __finished_tasks.size():
+		var task = __finished_tasks[t]
+		match task.tag:
+			tag:
+				result.append(task)
+			_:
+				new_finished_tasks.append(task)
+	__finished_tasks = new_finished_tasks
+	__finished_tasks_lock.unlock()
+	return result
 
 
 func print_thread_pool_info(arg=null) -> void:
